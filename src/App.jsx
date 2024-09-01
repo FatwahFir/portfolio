f1bfc6c8753c39389b5530c21d24274c0b1f5c6e
Fatwah Firmansyah <fatwah091@gmail.com>
2024-09-01 22:28:04 +0700

    navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,19 @@
+import "./app.scss"
+import Navbar from "./components/navbar/Navbar";
+
 const App = () => {
-  return <div>Hello World</div>;
+  return <div>
+    <section>
+      <Navbar/>
+    </section>
+    <section>Paralax</section>
+    <section>Services</section>
+    <section>Paralax</section>
+    <section>Port1</section>
+    <section>Port2</section>
+    <section>Port3</section>
+    <section>Contact</section>
+  </div>;
 };
 
 export default App;
