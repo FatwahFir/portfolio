f56e7941c1786a6cb127ab8281aced33f4b4f06a
Fatwah Firmansyah <fatwah091@gmail.com>
2024-09-02 12:13:27 +0700

    Hero

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -9,7 +9,7 @@ const Sidebar = ()=>{
 
     const variants={
         open:{
-            clipPath: 'circle(1200px at 50px 50px',
+            clipPath: 'circle(1200px at 50px 50px)',
             transition:{
                 type: 'spring',
                 stiffness: 20,
